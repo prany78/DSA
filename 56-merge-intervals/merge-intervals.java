b2ca9d2a7ba8158bb5101b8dd118ef5b4b9739d5
Pranjal Yadav <pranjalyadav7273@gmail.com>
2026-04-19 19:28:10 +0530

    Time: 8 ms (91.23%) | Memory: 49 MB (60.34%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<int[]> res = new ArrayList<>();
+
+        for(int[] interval : intervals) {
+            // if no overlap
+            if(res.isEmpty() || interval[0] > res.get(res.size() - 1)[1]) {
+                res.add(interval);
+            } 
+            else {
+                // merge
+                res.get(res.size() - 1)[1] = 
+                    Math.max(res.get(res.size() - 1)[1], interval[1]);
+            }
+        }
+
+        return res.toArray(new int[res.size()][]);
+    }
+}
